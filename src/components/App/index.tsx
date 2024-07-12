a2cfcd3b2c7e48eb84a659c08f5a29cad123cd22
Cheremis <alseta88@yandex.ru>
2024-07-12 21:34:15 +0300

    feat: simple styles and header

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,5 +1,13 @@
+import styles from './App.module.css';
+
 export default function App() {
   return (
-    <h1>Start coding!</h1>
-  )
+    <div className={styles.page}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>Сдержанное название</h1>
+        <p className={styles.slogan}>Кричащее описание произвольной длины</p>
+      </header>
+      <ul></ul>
+    </div>
+  );
 }
