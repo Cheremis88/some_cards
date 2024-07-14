1429398dc575aafc4fe0c2d4b508fb671d3b5499
Cheremis <alseta88@yandex.ru>
2024-07-15 00:16:31 +0300

    feat: filter likes, switcher

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,6 +1,6 @@
 import styles from './App.module.css';
 import { useDispatch } from '../../services/store';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchPosts } from '../../services/slice';
 import { useSelector } from '../../services/store';
 import { selectCards } from '../../services/slice';
@@ -10,19 +10,24 @@ export default function App() {
 
   const dispatch = useDispatch();
   const { cards, loading, error } = useSelector(selectCards);
+  const [likeFilter, setLikeFilter] = useState(false);
+  const filteredCards = likeFilter ? cards.filter(card => card.like) : cards;
 
   useEffect(() => {
     dispatch(fetchPosts());
   }, []);
-  
+
   return (
     <div className={styles.page}>
       <header className={styles.header}>
         <h1 className={styles.title}>Кричащее название</h1>
         <p className={styles.slogan}>Сдержанное описание произвольной длины</p>
       </header>
+      <label className={styles.switch}>
+        <input type="checkbox" className={styles.checkbox} onChange={() => setLikeFilter(!likeFilter)} />
+      </label>
       <ul className={styles.cardList}>
-        {cards.map(card =>
+        {filteredCards.map(card =>
           <Card key={card.id} card={card} />
         )}
       </ul>
